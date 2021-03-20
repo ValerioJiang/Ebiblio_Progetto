6ebaa44defc90dea6999b4e9f6125c0baedb0663
Valerio Jiang <jiangvalerio1998@gmail.com>
2021-03-20 12:08:12 +0100

    row on click biblioteche

--- biblioteche.php
+++ biblioteche.php
@@ -7,67 +7,71 @@ background: url('/ebiblio/images/scaffa.jpg') no-repeat  ;
 ">
 
 
-<div class="container" style="background-color: white;">
-  <br>
-  <!-- Nav tabs -->
-  <ul class="nav nav-tabs" role="tablist">
-    <li class="nav-item">
-      <a class="nav-link active" data-toggle="tab" href="#home">Biblioteca</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" data-toggle="tab" href="#menu1">Libro</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" data-toggle="tab" href="#menu2">Ebook</a>
-    </li>
-  </ul>
+  <div class="container" style="background-color: white;">
+    <br>
+    <!-- Nav tabs -->
+    <ul class="nav nav-tabs" role="tablist">
+      <li class="nav-item">
+        <a class="nav-link active" data-toggle="tab" href="#home">Biblioteca</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="tab" href="#menu1">Libro</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="tab" href="#menu2">Ebook</a>
+      </li>
+    </ul>
 
-  <!-- Tab panes -->
-  <div class="tab-content">
-    <div id="home" class="container tab-pane active"><br>
-    <div class="container">
-  <h2>Ricerca</h2>
-  </br>
-  <p>Nome</p>
-  <input type="text" placeholder="Inserire nome biblioteca">   
-  </br>
-  </br>
-  <table class="table table-hover">
-    <thead>
-      <tr>
-        <th>Nome</th>
-        <th>Indirizzo</th>
-      </tr>
-    </thead>
-    <tbody>
+    <!-- Tab panes -->
+    <div class="tab-content">
+      <div id="home" class="container tab-pane active"><br>
+        <div class="container">
+          <h2>Ricerca</h2>
+          </br>
+          <p>Nome</p>
+          <input type="text" placeholder="Inserire nome biblioteca">
+          </br>
+          </br>
+          <table class="table table-hover">
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>Indirizzo</th>
+              </tr>
+            </thead>
+            <tbody>
 
-      <?php
-        $biblioCon = new BibliotecaController();
-        $res = $biblioCon -> list();
-        
-        for($i = 0; $i < count($res);$i++){
-          echo '<tr>';  
-                echo '<td>'.$res[$i]['Nome'].'</td>';
-                echo '<td>'.$res[$i]['Indirizzo'].'</td>';
-            
-          echo '</tr>';
-        }
-      ?>
-      
-     </tbody>
-  </table>
-</div>
-    </div>
-    <div id="menu1" class="container tab-pane fade"><br>
-      <h3>Menu 1</h3>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-    </div>
-    <div id="menu2" class="container tab-pane fade"><br>
-      <h3>Menu 2</h3>
-      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+              <?php
+
+              $biblioCon = new BibliotecaController();
+              $res = $biblioCon->list();
+
+              for ($i = 0; $i < count($res); $i++) {
+                echo '<tr ' . 'onclick="window.location.assign(\'http://localhost/ebiblio/biblioinfo/' . $res[$i]['Nome'] . '\');"' . '>';
+                echo '<td>' . $res[$i]['Nome'] . '</td>';
+                echo '<td>' . $res[$i]['Indirizzo'] . '</td>';
+                echo '</tr>';
+              }
+              ?>
+              <tr onclick="window.location.assign('http://www.google.com');">
+                <td>ciao</td>
+                <td>uaaa</td>
+              </tr>
+
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div id="menu1" class="container tab-pane fade"><br>
+        <h3>Menu 1</h3>
+        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      </div>
+      <div id="menu2" class="container tab-pane fade"><br>
+        <h3>Menu 2</h3>
+        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+      </div>
     </div>
   </div>
-</div>
 
   <?php
   include('/xampp/htdocs/ebiblio/main_partials/footer.php');
