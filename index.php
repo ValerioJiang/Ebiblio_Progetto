2351933ef284edaf7abc7baae65443e9677f8f03
Valerio Jiang <jiangvalerio1998@gmail.com>
2021-03-20 10:24:07 +0100

    Index creation

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+include('./main_partials/menu.php')
+?>
+
+<div class="container-fluid " style="  
+background: url('./images/bibliofull.jpg') no-repeat  ;
+
+">
+<div class="row justify-content-center">
+  <div class="card" style="width: 60%;">
+    <div class="card-body p-5 align-self-center">
+    <h1 class="font-weight-light">Benvenuti in Ebiblio</h1>
+      <p class="lead">Il sistema di gestione biblioteche ufficiale UNIBO!</p>
+      <div style="height: 750px"></div>
+    </div>
+  </div>
+  </div>
+</div>
+
+
+<?php
+include('./main_partials/footer.php');
+?>
