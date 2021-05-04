14e148af3531e5d259bf3759002428d946474a95
Valerio Jiang <jiangvalerio1998@gmail.com>
2021-05-04 20:48:44 +0200

    stat

--- utilizzatore/statistiche_utilizzatore/postazione_statistiche.php
+++ utilizzatore/statistiche_utilizzatore/postazione_statistiche.php
@@ -20,39 +20,62 @@ background: url('/ebiblio/images/scaffa.jpg') no-repeat  ;
 
   <div class="container" style="background-color: white;">
     <br>
-        <div class="container">
+      <div class="container">
           <h2>Classifiche</h2>
           </br>
           </br>
-          <h5>Classifica posti lettura più usati</h5> 
+          <h5>Classifica posti lettura meno utilizzati</h5> 
 
          
           <table class="table table-hover">
             <thead>
               <tr>
                 <th>Biblioteca</th>
-                <th>Posti totali</th>
-                <th>Percentuale posti usati</th>
+                <th>Percentuale posti usati rispetto al totale</th>
 
                 <?php
                   
-               $biblio_res = $bibliotecaCon->list();
+                  $biblio_res = $bibliotecaCon->list();
+                  $arr_res = array();
+                  
+                  for($j = 0; $j < count($biblio_res); $j++){
+                    
+                    $prenotazione_res=$prenotazioneCon->createStatisticaPosto($biblio_res[$j]['Nome']);
 
-                  for ($i = 0; $i < count($biblio_res); $i++) {
-                    $denominatore_res = $postoCon->getDenominatoreStatistica();                  
+                    if($prenotazione_res[0]['Biblioteca']== NULL||$prenotazione_res[0]['Percentuale']==NULL){
+
+                    }
+                    else{
+                      $arr_res[]=$prenotazione_res[0];
+                    }
                   }
-                
-                  $prenotazione_res=$prenotazioneCon->createStatisticaPosto( $denominatore_res[0]['den'],$denominatore_res[0]['Biblioteca']);
 
-                  for($i = 0; $i < count($prenotazione_res); $i++) {
-
-                    echo '<tr>';   
-                    echo '<td>' . $prenotazione_res[$i]['Biblioteca'] . '</td>';
-                    echo '<td>' . $denominatore_res[$i]['den'] . '</td>';
-                    echo '<td>' . $prenotazione_res[$i]['percentuale'] . '%</td>';
-                    echo '</tr>';
+                  
+                  for($i=0; $i<count($arr_res); $i++){
+                    $min = $i;
+                      for($k = $i+1; $k <count($arr_res); $k++){
+                        if((float)$arr_res[$k]['Percentuale'] < (float)$arr_res[$min]){
+                            $min =  $k;
+                        }
+                      }
+                      $temp = $arr_res[$min];
+                      $arr_res[$min] = $arr_res[$i];
+                      $arr_res[$i] = $temp;
                   }
-                
+                  
+                  for($l = 0; $l < count($arr_res); $l++){
+                    echo '<tr>';
+                    echo '<td>'.$arr_res[$l]['Biblioteca'].'<td>';
+                    echo '<td>'.$arr_res[$l]['Percentuale'].'%<td>';
+                    echo '<tr>';
+                  }
+              
+
+                  
+
+                  
+
+
               
                 ?>
               </tr>
@@ -61,10 +84,6 @@ background: url('/ebiblio/images/scaffa.jpg') no-repeat  ;
 
             </tbody>
           </table>
-
-
-
-
 
     </div>
   </div>
